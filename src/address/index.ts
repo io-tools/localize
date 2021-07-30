9a6722ac7d74b8baa2fd72f5781579c347be037c
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2021-07-30 21:15:34 +0300

    First test

--- src/address/index.ts
+++ src/address/index.ts
@@ -1,3 +1,7 @@
-export class Address {}
+export class Address {
+  public hello(name: string): void {
+    console.log(`Hello, ${name}`);
+  }
+}
 
 export default Address;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import { Address } from "./address";
 
 export { Address };
+
